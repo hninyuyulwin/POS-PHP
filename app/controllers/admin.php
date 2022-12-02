d6dc8ce0744a0bf0be93c07b2efb2c450f0b8e7a
Rosie Mary <laltnadi@gmail.com>
2022-12-02 13:05:49 +0630

    Adding Query By Date

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -14,25 +14,45 @@ if ($tab == 'products') {
   $offset = $pager->offset;
   $users = $userClass->query("SELECT * FROM users ORDER By id DESC limit $limit offset $offset");
 } else if ($tab == 'sales') {
+
+  $session = $_GET['s'] ?? 'table';
+  $startdate = $_GET['start'] ?? null;
+  $enddate = $_GET['end'] ?? null;
+
   $saleClass = new Sale();
-  $limit = 5;
+  $limit = $_GET['limit'] ?? 10;
+  $limit = (int)$limit;
+  $limit = ($limit < 1) ? 10 : $limit;
+
   $pager = new Pager($limit);
   $offset = $pager->offset;
-  $sales = $saleClass->query("SELECT * FROM sales ORDER By id DESC limit $limit offset $offset");
+  $query = "SELECT * FROM sales ORDER By id DESC limit $limit offset $offset";
 
   // get today's sales total
   $year = date("Y");
   $month = date("m");
   $day = date("d");
-  $query = "SELECT sum(total) as total FROM sales WHERE day(date) = $day && month(date) = $month && year(date) = $year";
+  $query_total = "SELECT sum(total) as total FROM sales WHERE day(date) = $day && month(date) = $month && year(date) = $year";
 
-  $st = $saleClass->query($query);
+
+  // if both startdate and enddate contain
+  if ($startdate && $enddate) {
+    $query = "SELECT * FROM sales WHERE date BETWEEN '$startdate' and '$enddate' ORDER By id DESC limit $limit offset $offset";
+    $query_total = "SELECT sum(total) as total FROM sales WHERE date between '$startdate' and '$enddate'";
+  }
+  // if only startdate is set
+  else if ($startdate && !$enddate) {
+    $query = "SELECT * FROM sales WHERE date = '$startdate' ORDER By id DESC limit $limit offset $offset";
+    $query_total = "SELECT sum(total) as total FROM sales WHERE date = '$startdate'";
+  }
+  $sales = $saleClass->query($query);
+
+  $st = $saleClass->query($query_total);
   $sales_total = 0;
   if ($st) {
     $sales_total = $st[0]['total'] ?? 0;
   }
 }
-
 
 if (Auth::access('supervisor')) {
   require views_path('admin/admin');
--- app/views/admin/sales.view.php
+++ app/views/admin/sales.view.php
@@ -1,76 +1,98 @@
 <ul class="nav nav-tabs">
   <li class="nav-item">
-    <a href="#" class="nav-link active" aria-current="page">Table View</a>
+    <a href="index.php?pg=admin&tab=sales" class="nav-link <?php echo ($session == 'table') ? 'active' : ''; ?>" aria-current="page">Table View</a>
   </li>
   <li class="nav-item">
-    <a href="#" class="nav-link">Graph View</a>
+    <a href="index.php?pg=admin&tab=sales&s=graph" class="nav-link  <?php echo ($session == 'graph') ? 'active' : ''; ?>">Graph View</a>
   </li>
 </ul>
-<div class="table-responsive">
-  <h4 class="my-3 text-success">Today Sales Total : <?= number_format($sales_total) . " MMK"; ?></h4>
-  <table class="table table-hovered table-primary">
-    <?php
-    $id = 1;
-    if (!empty($sales)) :
-    ?>
-      <tr>
-        <th>#</th>
-        <th>Barcode</th>
-        <th>Recipt No</th>
-        <th>Description</th>
-        <th>Qty</th>
-        <th>Amount</th>
-        <th>Total</th>
-        <th>Date</th>
-        <th>Seller Name</th>
-        <th>Action</th>
-      </tr>
+<?php if ($session == 'table') : ?>
+  <div class="table-responsive">
+    <form action="" class="mt-3">
+      <input type="hidden" name="pg" value="admin">
+      <input type="hidden" name="tab" value="sales">
+      <div class="row">
+        <div class="col-3">
+          <label for="start">Start Date :</label>
+          <input type="date" name="start" id="start" class="form-control" value="<?= !empty($_GET['start']) ? $_GET['start'] : ''; ?>">
+        </div>
+        <div class="col-3">
+          <label for="end">End Date :</label>
+          <input type="date" name="end" id="end" class="form-control" value="<?= !empty($_GET['end']) ? $_GET['end'] : ''; ?>">
+        </div>
+        <div class="col-3">
+          <label for="limit">Rows :</label>
+          <input type="number" name="limit" id="limit" min="1" class="form-control" value="<?= !empty($_GET['limit']) ? $_GET['limit'] : '10'; ?>">
+        </div>
+      </div>
+      <button type="submit" class="btn btn-sm btn-primary mt-1">Go <i class="fa fa-chevron-right"></i></button>
+    </form>
+    <h4 class="my-3 text-success">Today Sales Total : <?= number_format($sales_total) . " MMK"; ?></h4>
+    <table class="table table-hovered table-primary">
       <?php
-      foreach ($sales as $sale) :
+      $id = 1;
+      if (!empty($sales)) :
       ?>
         <tr>
-          <td><?php echo $id; ?></td>
-          <td><?php echo esc($sale['barcode']); ?></td>
-          <td><?php echo esc($sale['recipt_no']); ?></td>
-          <td><?php echo esc($sale['description']); ?></td>
-          <td><?php echo esc($sale['qty']); ?></td>
-          <td><?php echo esc($sale['amount']); ?></td>
-          <td><?php echo esc($sale['total']); ?></td>
-          <td><?php echo date("jS M, Y", strtotime($sale['date'])); ?></td>
-          <?php
-          $cashier = get_user_by_id($sale['user_id']);
-          if (empty($cashier)) {
-            $name = "Unknown User";
-            $role = "Unknown Role";
-            $nameLink = "#";
-          } else {
-            $name = $cashier['username'];
-            $role = $cashier['role'];
-            $nameLink = "index.php?pg=profile&id=" . $cashier['id'];
-          }
-          ?>
-          <td>
-            <a href="<?= $nameLink ?>"><?php echo esc($name) . "(" . $role . ")"; ?></a>
-          </td>
-          <td>
-            <a href="index.php?pg=sales-delete&id=<?= $sale['id']; ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
+          <th>#</th>
+          <th>Barcode</th>
+          <th>Recipt No</th>
+          <th>Description</th>
+          <th>Qty</th>
+          <th>Amount</th>
+          <th>Total</th>
+          <th>Date</th>
+          <th>Seller Name</th>
+          <th>Action</th>
         </tr>
+        <?php
+        foreach ($sales as $sale) :
+        ?>
+          <tr>
+            <td><?php echo $id; ?></td>
+            <td><?php echo esc($sale['barcode']); ?></td>
+            <td><?php echo esc($sale['recipt_no']); ?></td>
+            <td><?php echo esc($sale['description']); ?></td>
+            <td><?php echo esc($sale['qty']); ?></td>
+            <td><?php echo esc($sale['amount']); ?></td>
+            <td><?php echo esc($sale['total']); ?></td>
+            <td><?php echo date("jS M, Y", strtotime($sale['date'])); ?></td>
+            <?php
+            $cashier = get_user_by_id($sale['user_id']);
+            if (empty($cashier)) {
+              $name = "Unknown User";
+              $role = "Unknown Role";
+              $nameLink = "#";
+            } else {
+              $name = $cashier['username'];
+              $role = $cashier['role'];
+              $nameLink = "index.php?pg=profile&id=" . $cashier['id'];
+            }
+            ?>
+            <td>
+              <a href="<?= $nameLink ?>"><?php echo esc($name) . "(" . $role . ")"; ?></a>
+            </td>
+            <td>
+              <a href="index.php?pg=sales-delete&id=<?= $sale['id']; ?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
+          </tr>
+        <?php
+          $id++;
+        endforeach;
+      else :
+        ?>
+        <div class="alert alert-warning text-center">
+          <h3>No Sales Items Yet!!</h3>
+        </div>
       <?php
-        $id++;
-      endforeach;
-    else :
+      endif;
       ?>
-      <div class="alert alert-warning text-center">
-        <h3>No Sales Items Yet!!</h3>
-      </div>
+    </table>
+
     <?php
-    endif;
+    echo $pager->display();
     ?>
-  </table>
-
-  <?php
-  echo $pager->display();
-  ?>
-
-
-</div>
+  </div>
+<?php else : ?>
+  <h3>This is the graph view</h3>
+  <svg></svg>
+<?php endif; ?>
